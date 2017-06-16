735b8079061a57bcf958d9654376b87ad3c03a9d
Bruno <iam@corrni.io>
2017-06-16 19:05:53 +0200

    Using cached data

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -22,26 +22,12 @@ self.addEventListener('install', function(event) {
 })
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request)
   self.__CURRENT_FETCH__ = event
 
-  if (event.request.url.endsWith('jpg')) {
-    event.respondWith(fetch('/imgs/dr-evil.gif'))
-  } else {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          if (response.status === 404) {
-            // return new Response("Whoops, not found :(")
-
-            // NOTE: you can also return a `fetch`ed promise
-            return fetch('/imgs/dr-evil.gif')
-          }
-          return response
-        })
-        .catch(() => new Response("Uh oh, that totally failed!"))
-    )
-  }
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response
+      return fetch(event.request)
+    })
+  )
 })
-
-/// HELPERS ///
