292f2171e3e6439a68aa639e6f49fa071cfac3ee
Bruno <iam@corrni.io>
2017-06-16 17:38:19 +0200

    conditional responses - checking request's URL

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,17 +2,32 @@ self.addEventListener('fetch', function(event) {
   console.log(event.request)
   // self.__CURRENT_FETCH__ = event
 
-  const html = `
-  <div class="a-winner-is-me">
-    <h1>Hello World!</h1>
-  </div>
-  `
-
-  event.respondWith(
-    new Response(html, {
-      headers: {
-        'Content-Type': 'text/html;charset=UTF-8'
-      }
-    })
-  )
+  if (checkUrlforJpg(event.request.url)) {
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    )
+  }
 })
+
+
+/// HELPERS ///
+
+function checkUrlforJpg(url) {
+  console.log(url.endsWith('jpg'))
+  const split = url.split('.')
+  return 'jpg' === split.reverse()[0]
+}
+
+function fixedHello(respBody) { // eslint-disable-line no-unused-vars
+  const html = respBody || `
+    <div class="a-winner-is-me">
+      <h1>Hello World!</h1>
+    </div>
+    `
+
+  new Response(html, {
+    headers: {
+      'Content-Type': 'text/html;charset=UTF-8'
+    }
+  })
+}
