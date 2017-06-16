a97a87eaecbb2226021deb9ecf21c074f1057dcf
Bruno <iam@corrni.io>
2017-06-16 18:56:55 +0200

    sw installation, storing objects to cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,26 @@
+self.addEventListener('install', function(event) {
+  const urlsToCache = [
+    '/',
+    '/js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+  ]
+
+  event.waitUntil(
+    // NOTE: save urls to 'wittr-static-v1' cache
+    caches.open('wittr-static-v1').then(function(cache) {
+      console.groupCollapsed(`SW setup -- caching ${urlsToCache.length} objects`)
+      console.info('Successful SW Installation')
+      console.info('URLs to cache: ', urlsToCache)
+      console.groupEnd()
+
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
 self.addEventListener('fetch', function(event) {
   console.log(event.request)
   self.__CURRENT_FETCH__ = event
