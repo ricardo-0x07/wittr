8a044c9ba5f8e3206bb7dcf89d5fa4edf3765019
Bruno <iam@corrni.io>
2017-06-17 02:36:10 +0200

    people store

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,8 +1,21 @@
-import idb from 'idb'
+// @flow
+/* eslint-disable no-case-declarations, no-fallthrough */
 
-const DB = idb.open('test-db', 1, function(upgradeDb) {
-  const kvStore = upgradeDb.createObjectStore('keyval')
-  kvStore.put('world', 'hello')
+import idb from 'idb'
+import people from './people'
+
+const DB = idb.open('test-db', 3, function(upgradeDb) {
+  // NOTE: `upgradeDB.oldVersion` is how you deal with multiple versions
+  switch(upgradeDb.oldVersion) {
+    case 0:
+      const kvStore = upgradeDb.createObjectStore('keyval')
+      kvStore.put('world', 'hello')
+    case 1:
+      upgradeDb.createObjectStore('people', {keyPath: 'name'})
+    case 2:
+      const peopleStore = upgradeDb.transaction.objectStore('people')
+      peopleStore.createIndex('animal', 'favoriteAnimal')
+  }
 })
 
 // Reading from the DB
@@ -31,3 +44,12 @@ DB.then(db => {
   store.put('donkey', 'favoriteAnimal')
   return tx.complete
 }).then(() => console.log("Added favoriteAnimal:donkey to 'keyval' store"))
+
+// Add people to "people" store
+DB.then(db => {
+  const tx = db.transaction('people', 'readwrite')
+  const store = tx.objectStore('people')
+  people.forEach(p => store.put(p))
+
+  return tx.complete
+}).then(() => console.log('People added'))
