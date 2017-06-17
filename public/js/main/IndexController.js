327e1e22a9685dbb1c1165f1e9f43b41bf3f67f3
Bruno <iam@corrni.io>
2017-06-17 22:20:02 +0200

    Improve IndexController's type-safety

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -1,73 +1,101 @@
+// @flow weak
+
+import idb from 'idb'
+import invariant from 'invariant'
 import PostsView from './views/Posts'
 import ToastsView from './views/Toasts'
-import idb from 'idb'
 
-export default function IndexController(container) {
+const NO_SW_MESSAGE = 'Service Workers are not supported on this browser!'
+
+function openDatabase() {
+  // If the browser doesn't support service worker,
+  // we don't care about having a database
+  if (!navigator.serviceWorker) {
+    return Promise.resolve()
+  }
+
+  // TODO: return a promise for a database called 'wittr'
+  // that contains one objectStore: 'wittrs'
+  // that uses 'id' as its key
+  // and has an index called 'by-date', which is sorted
+  // by the 'time' property
+}
+
+export default function IndexController(container: HTMLElement) {
+  invariant(container, "The `container` provided is not an HTML Element!")
+
   this._container = container
   this._postsView = new PostsView(this._container)
   this._toastsView = new ToastsView(this._container)
   this._lostConnectionToast = null
   this._openSocket()
-  this._registerServiceWorker()
+  this._dbPromise = openDatabase()
+
+  if ('serviceWorker' in navigator) {
+    this._registerServiceWorker()
+  } else {
+    console.warn(NO_SW_MESSAGE)
+  }
 }
 
 IndexController.prototype._registerServiceWorker = function() {
   const indexController = this
 
-  if (!navigator.serviceWorker) {
-    console.error('Service Workers are not supported on this browser.')
-    return
-  }
+  invariant(navigator.serviceWorker, NO_SW_MESSAGE)
 
-  navigator.serviceWorker.register('/sw.js')
-    .then(function(reg) {
-      self.__SW_REGISTRATION__ = reg // DEBUG ONLY
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    if (process.env.NODE_ENV === 'development')
+      self.__SW_REGISTRATION__ = reg
 
-      // NOTE/FSO: no controller means page wasn't loaded via a SW
-      // (so users are looking the latest version)
-      if (!navigator.serviceWorker.controller) return
+    // $FlowFixMe -- NOTE: Not loaded via SW, so we know it's the latest version
+    if (!Boolean(navigator.serviceWorker.controller)) // eslint-disable-line no-extra-boolean-cast
+      return
 
-      // updated worker already waiting
-      if (reg.waiting) {
-        indexController._updateReady(reg.waiting)
-        return
-      }
+    // updated worker already waiting
+    if (reg.waiting) {
+      indexController._updateReady(reg.waiting)
+      return
+    }
 
-      // NOTE: if there's an updated worker installing, track its
-      // progress. If it becomes "installed", call
-      // indexController._updateReady()
-      if (reg.installing) {
-        indexController._trackInstalling(reg.installing)
-        return
-      }
+    // NOTE: if there's an updated worker installing, track its
+    // progress. If it becomes "installed", call
+    // indexController._updateReady()
+    if (reg.installing) {
+      indexController._trackInstalling(reg.installing)
+      return
+    }
 
-      // [...] otherwise, listen for new installing workers arriving, and
-      // track their progress
-      reg.addEventListener('updatefound', () => {
-        indexController._trackInstalling(reg.installing)
-      })
+    // [...] otherwise, listen for new installing workers arriving and track their progress
+    reg.addEventListener('updatefound', function() {
+      indexController._trackInstalling(reg.installing)
     })
+  })
 
-  // listen for controlling SW changing & and reload the page
+  // Listen for controlling SW changies & reload when appropriate.
+  let refreshing = false
+  // $FlowFixMe
   navigator.serviceWorker.addEventListener('controllerchange', (event) => {
-    console.log('[sw#controllerchange]', event)
+    if (process.env.NODE_ENV === 'development')
+      console.log('[sw#controllerchange]', event)
+
+    if (refreshing) return // ensure `refresh` is only called once.
     window.location.reload()
+    refreshing = true
   })
 }
 
 // Keep track of a Service Worker that's being installed, and
 // notify the user when the installation has successfully finished.
-IndexController.prototype._trackInstalling = function(worker) {
+IndexController.prototype._trackInstalling = function(worker: ServiceWorker) {
   const indexController = this
-
   worker.addEventListener('statechange', () => {
     if (worker.state === 'installed') {
-      indexController._updateReady()
+      indexController._updateReady(worker)
     }
   })
 }
 
-IndexController.prototype._updateReady = function(worker, updateMessage) {
+IndexController.prototype._updateReady = function(worker: ServiceWorker, updateMessage?: string) {
   const msg = updateMessage || 'New Version Available'
   const toast = this._toastsView.show(msg, {
     buttons: ['refresh', 'dismiss']
