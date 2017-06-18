8052404c04c108780fdad94f34e4fe2343fd91a6
Bruno <iam@corrni.io>
2017-06-18 02:41:40 +0200

    Service photos from cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,13 @@
 // @flow
 
-const STATIC_CACHE_NAME = 'wittr-static-v7'
+const STATIC_CACHE_NAME = 'wittr-static-v8'
+const CONTENT_IMG_CACHE = 'wittr-content-imgs'
+
+// all the caches we care about
+const allCaches = [
+  STATIC_CACHE_NAME,
+  CONTENT_IMG_CACHE,
+]
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -19,13 +26,11 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    // NOTE: Only remove caches beginning with 'wittr-', and don't disturb
-    // caches which other parts of the app may depend on.
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.filter(cacheName => {
           return cacheName.startsWith('wittr-') &&
-                 cacheName != STATIC_CACHE_NAME
+                 !allCaches.includes(cacheName)
         }).map(cacheName => caches.delete(cacheName))
       )
     })
@@ -35,11 +40,18 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event: FetchEvent) {
   const requestUrl = new URL(event.request.url)
 
+  // app shell
   if (requestUrl.origin === location.origin) {
     if (requestUrl.pathname === '/') {
       event.respondWith(caches.match('/skeleton'))
-      return // `skeleton` is cached at install time, so we don't need fallback
+      return
     }
+  }
+
+  // photos (non-static content)
+  if (requestUrl.pathname.startsWith('/photos/')) {
+    event.respondWith(servePhoto(event.request))
+    return
   }
 
   event.respondWith(
@@ -48,6 +60,31 @@ self.addEventListener('fetch', function(event: FetchEvent) {
     })
   )
 })
+
+function servePhoto(request: Request) {
+  // Photo urls look like:
+  // /photos/9-8028-7527734776-e1d2bda28e-800px.jpg
+  // But storageUrl has the -800px.jpg bit missing.
+  // Use this url to store & match the image in the cache.
+  // This means you only store one copy of each photo.
+  const storageUrl = request.url.replace(/-\d+px\.jpg$/, '')
+
+  // NOTE: return images from the "wittr-content-imgs" cache
+  // if they're in there. Otherwise, fetch the images from
+  // the network, put them into the cache, and send it back
+  // to the browser.
+  //
+  // HINT: cache.put supports a plain url as the first parameter
+  return caches.open(CONTENT_IMG_CACHE)
+    .then(cache => cache.match(storageUrl).then(function(response) {
+      if (response) return response
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone())
+        return networkResponse
+      })
+    }))
+}
 
 // `skipWaiting` if we get the right kind of message from the UI
 self.addEventListener('message', function(event) {
