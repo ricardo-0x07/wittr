b44059337b72ac36cdf62f74043c7fd9ce1bfe2c
Bruno <iam@corrni.io>
2017-06-19 03:30:28 +0200

    Type-safe parseHTML utility

--- public/js/utils/parseHTML.js
+++ public/js/utils/parseHTML.js
@@ -1,6 +1,13 @@
-var contextRange = document.createRange()
-contextRange.setStart(document.body, 0)
+// @flow
 
-export default function strToEls(str) {
+const contextRange = (function createCtxRange(bodyRef) {
+  if (!bodyRef) throw Error("Can't work with empty document.body!")
+
+  const ctxRange = document.createRange()
+  ctxRange.setStart(bodyRef, 0)
+  return ctxRange
+})(document.body)
+
+export default function strToEls(str: string) {
   return contextRange.createContextualFragment(str)
 }
